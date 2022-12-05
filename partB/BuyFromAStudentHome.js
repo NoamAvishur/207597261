2b50faf36adc4a4366e1531d9e82d31cfb44b929
NoamAvishur <118104112+NoamAvishur@users.noreply.github.com>
2022-12-05 15:14:33 +0200

    Add files via upload

---
+++ partB/BuyFromAStudentHome.js
@@ -0,0 +1,25 @@
+const myForm = document.querySelector('.my-form')
+const emailInput = document.querySelector('#email')
+const passwordInput = document.querySelector('#password')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('.users')
+const onSubmit = (e) => {
+    e.preventDefault()
+    if (emailInput.value === '' || passwordInput.value === '' ){
+        console.log('error')
+        msg.innerHTML = 'בבקשה הכנס ערך בכל השדות'
+        msg.classList.add('error')
+    } else {
+        console.log('success')
+        const li = document.createElement('li')
+        li.innerHTML = `${passwordInput.value}: ${emailInput.value}`
+        //userList.appendChild(li)
+        // clean fields
+        passwordInput.value = ''
+        emailInput.value = ''
+        msg.innerHTML = ''
+        msg.classList.remove('error')
+
+    }
+}
+myForm.addEventListener('submit', onSubmit)
