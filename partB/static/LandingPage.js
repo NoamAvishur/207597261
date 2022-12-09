a66f649040d536d37813fe2251b81f0e8eb3979b
NoamAvishur <118104112+NoamAvishur@users.noreply.github.com>
2022-12-09 11:20:24 +0200

    Add files via upload

--- partB/static/LandingPage.js
+++ partB/static/LandingPage.js
@@ -1,14 +1,11 @@
 const myForm = document.querySelector('.my-form')
-const firstNameInput=document.querySelector(`#firstName`)
-const lastNameInput=document.querySelector(`#lastName`)
 const emailInput = document.querySelector('#email')
 const passwordInput = document.querySelector('#password')
 const msg = document.querySelector('.msg')
 const userList = document.querySelector('.users')
-
 const onSubmit = (e) => {
     e.preventDefault()
-    if (emailInput.value === '' || passwordInput.value === '' || firstNameInput.value === '' || lastNameInput.value === '' ){
+    if (emailInput.value === '' || passwordInput.value === '' ){
         console.log('error')
         msg.innerHTML = 'בבקשה הכנס ערך בכל השדות'
         msg.classList.add('error')
@@ -17,15 +14,13 @@ const onSubmit = (e) => {
         const li = document.createElement('li')
         li.innerHTML = `${passwordInput.value}: ${emailInput.value}`
         //userList.appendChild(li)
+        window.location.href="../views/Seller.html"
         // clean fields
-        firstNameInput.value = ''
-        lastNameInput.value = ''
         passwordInput.value = ''
         emailInput.value = ''
         msg.innerHTML = ''
         msg.classList.remove('error')
-        window.location.href="../views/Seller.html"
+
     }
 }
-
 myForm.addEventListener('submit', onSubmit)
