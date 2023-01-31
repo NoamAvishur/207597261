75421604d5676a5d400bf5a92e116e60a4939be4
NoamAvishur <118104112+NoamAvishur@users.noreply.github.com>
2023-01-31 20:15:51 +0200

    Add files via upload

--- partC/db/CRUD.js
+++ partC/db/CRUD.js
@@ -1,20 +1,155 @@
-const sql = require('./db');
+const sql = require('./db'); //DAY 11 B
 
-const select_all = (req,res)=>{
+const insertNewSignIN = (req,res)=>{
+    // validate body exists
+    if (!req.body) {
+        res.status(400).send({message: "content cannot be empty"});
+        return;
+    }
+    // insert input data from body into json
+    const NewSignUp = {
+        "email": req.body.SignUpEmail,
+        "name": req.body.SignUpName
+    }
+    // run qury
+    const Q1 = 'INSERT INTO customers SET ?';
+    sql.query(Q1, NewSignUp, (err, mysqlres) =>{
+        if (err) {
+            console.log("error: error: ", err);
+            res.status(400).send({message:"couild not sign in"});
+            return;
+        }
+        console.log("created customer: ", {id: mysqlres.insertid});
+        res.send({message: "you just signed in successfuly"});
+        return;
+    })
+};
+
+const showAll = (req,res)=>{
     const Q2 = "SELECT * from customers";
-
     sql.query(Q2, (err, mysqlres)=>{
         if (err) {
             res.status(400).send("error");
             return;
         };
         //res.send(mysqlres);
-        res.render('mysqlres', {
-            v1: 'All users',
-            pple: mysqlres
+        const t = new Date();
+        res.render('results', {
+            v1:'Customers',
+            v2:t,
+            mysqlresArray:mysqlres
         })
         return;
     });
 };
 
-module.exports= {select_all}
+const findUser = (req,res)=>{
+    // validate body exists
+    if (!req.body) {
+        res.status(400).send({message: "please fill user name to search"});
+        return;    }
+    // pull data from body
+    const user = req.body.SearchName;
+
+    // run query
+    const Q3 = "SELECT * FROM customers where name like ?";
+    sql.query(Q3, user + "%", (err, mysqlres)=>{
+        if (err) {
+            console.log("error: error: ", err);
+            res.status(400).send({message:"could not search customer"});
+            return;
+        }
+        console.log("found user: ", mysqlres);
+        res.send(mysqlres);
+        return;
+    })
+}
+
+module.exports = {insertNewSignIN, showAll, findUser};
+
+var sql = require('./db');
+const path = require('path');
+const csv=require('csvtojson');
+const cookieParser = require('cookie-parser');
+
+
+const UserSignUp = (req,res)=>{
+    // validate body exists
+    if (!req.body) {
+        res.status(400).send('error' ,{message: "content cannot be empty"});
+        return;
+    }
+    // create timestamp
+    let d = new Date();    
+    // insert input data from body into json
+    const NewUserSignIn = {
+        "UserName": req.body.UserName,
+        "UserPassword": req.body.UserPassword,
+        "TimeStamp": d
+    }
+    res.cookie("Signed_user", req.body.UserName);
+
+    // run qury
+    const Q1 = 'INSERT INTO TableA SET ?';
+    sql.query(Q1, NewUserSignIn, (err, mysqlres) =>{
+        if (err) {
+            console.log("error: error: ", err);
+            res.status(400).render('error' , {message:"could not sign up"});
+            return;
+        }
+        res.redirect('/signedUp');
+        return;
+    })
+};
+
+const showAllEntries = (req,res)=>{
+
+  // pull data from body
+  const user = req.cookies.Signed_user;
+  // run query
+  // const Q3 = "SELECT * FROM customers where name like ?";
+  const Q3 = 'SELECT * FROM TableA where UserName like ?'
+  sql.query(Q3, user + '%',(err, mysqlres)=>{
+      if (err) {
+          console.log("error: error: ", err);
+          res.status(400).send({message:"could not search customer"});
+          return;
+      }
+      console.log("found user: ", mysqlres);
+      res.render('page2',{
+        v1: 'All entries by '+ user,
+        v2:mysqlres
+    });
+      return;
+  })
+};
+
+
+const UserSignIn = (req, res) => {
+    // get the data
+    const userName = req.cookies.Signed_user;
+  
+    // fake test data
+    let userdetails = {
+      username: "Bob",
+      password: "123456",
+    };
+  
+    // basic check
+    if (
+      userName === userdetails["username"] 
+    ) {
+      // saving the data to the cookies
+      res.cookie("username", userName);
+      // redirect
+      return res.render("welcome", {
+        v1: userName
+      });
+    } else {
+      // redirect with a fail msg
+      return res.render('error', {message: "some random message"});
+    }
+  };
+
+
+module.exports = {UserSignUp, showAllEntries, UserSignIn};
--- partC/index.js
+++ partC/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const BodyParser = require('body-parser');
 //const cookieParser = require('cookie-parser');
-//const sql = require('./db/db');
-//const CreateDB = require('./db/CreateDB');
-//const CRUD = require('./db/CRUD');
+const sql = require('./db/db');
+const CreateDB = require('./db/CreateDB');
+const CRUD = require('./db/CRUD');
 const port = 3000;
+const fs = require('fs');
+const stringify = require('csv-stringify').stringify;
+const { parse } = require("csv-parse");
+const CSVToJSON = require('csvtojson');// DAY 12 MANAGE CONTENT
 
 const app = express();
 app.use(BodyParser.json());
@@ -15,6 +19,22 @@ app.use(express.static(path.join(__dirname,'static')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.get('/show_all_customers', CRUD.showAll); // 11 B
+
+app.get('/CreateTable',CreateDB.CreateTable);
+
+app.get("/InsertData", CreateDB.InsertData);
+
+app.get('/ShowTable', CreateDB.ShowTable);
+
+app.get('/DropTable', CreateDB.DropTable);
+
+app.get('/signedUp', (req,res)=>{
+    let userNameCookie = req.cookies.Signed_user;
+    console.log(userNameCookie);
+    res.render('welcome', {v1: userNameCookie});
+});
 
 app.get('/', (req, res)=>{
     res.redirect('/landingPage');
@@ -28,11 +48,11 @@ app.get('/home', (req, res)=>{
     res.render('home');
 });
 
-app.get('/results', (req, res)=>{
+app.get('/results', (req, res)=>{// DAY 12 MANAGE CONTENT
     res.render('results');
 });
 
-app.get('/product', (req, res)=>{
+app.get('/product', (req, res)=>{// DAY 12 MANAGE CONTENT
     res.render('product');
 });
 
@@ -51,7 +71,7 @@ app.get('/createaccount', (req, res)=>{
     res.render('createaccount');
 });
 
-app.get('/seller', (req, res)=>{
+app.get('/seller', (req, res)=>{// DAY 12 MANAGE CONTENT
     res.render('seller');
 });
 
