29d78e3e053adf9d719e639d195614dd6df43365
NoamAvishur <118104112+NoamAvishur@users.noreply.github.com>
2023-01-31 13:23:18 +0200

    Add files via upload

--- partC/index.js
+++ partC/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const BodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const sql = require('./db/db');
+//const cookieParser = require('cookie-parser');
+//const sql = require('./db/db');
 //const CreateDB = require('./db/CreateDB');
-const CRUD = require('./db/CRUD');
-const port = 8080;
+//const CRUD = require('./db/CRUD');
+const port = 3000;
 
 const app = express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'static')));
-app.use(cookieParser());
+//app.use(cookieParser());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,8 +22,21 @@ app.get('/', (req, res)=>{
 app.get('/landingPage', (req, res)=>{
     res.render('landingpage');
 });
+app.get('/home', (req, res)=>{
+    res.render('home');
+});
+app.get('/contact', (req, res)=>{
+    res.render('contact');
+});
 
-app.listen(8080, () => {
+app.get('/about', (req, res)=>{
+    res.render('about',{
+        V1:'אודותינו',
+        V2:''
+    });
+});
+
+app.listen(port, () => {
     console.log("Server is running on port ", port
     );
-    });
+});
