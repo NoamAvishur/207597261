ae60a181d84044d46d59943a44263d3c2df1ac4c
NoamAvishur <118104112+NoamAvishur@users.noreply.github.com>
2023-01-31 18:32:31 +0200

    Add files via upload

--- partC/index.js
+++ partC/index.js
@@ -19,26 +19,48 @@ app.set('view engine', 'pug');
 app.get('/', (req, res)=>{
     res.redirect('/landingPage');
 });
+
 app.get('/landingPage', (req, res)=>{
     res.render('landingpage');
 });
+
 app.get('/home', (req, res)=>{
     res.render('home');
 });
-app.get('/contact', (req, res)=>{
-    res.render('contact');
+
+app.get('/results', (req, res)=>{
+    res.render('results');
 });
-app.get('/createaccount', (req, res)=>{
-    res.render('createaccount');
+
+app.get('/product', (req, res)=>{
+    res.render('product');
 });
-app.get('/seller', (req, res)=>{
-    res.render('seller');
-});
+
 app.get('/about', (req, res)=>{
     res.render('about',{
         V1:'אודותינו',
         V2:''
     });
+});
+
+app.get('/contact', (req, res)=>{
+    res.render('contact');
+});
+
+app.get('/createaccount', (req, res)=>{
+    res.render('createaccount');
+});
+
+app.get('/seller', (req, res)=>{
+    res.render('seller');
+});
+
+app.get('/createproduct', (req, res)=>{
+    res.render('createproduct');
+});
+
+app.get('/editproduct', (req, res)=>{
+    res.render('editproduct');
 });
 
 app.listen(port, () => {
